a3b8c18253308f8687f91e7ba01716b1f9fb1718
Violetta-321 <113465444+Violetta-321@users.noreply.github.com>
2022-09-13 23:19:25 +0300

    Create btcBuy.js

---
+++ btcBuy.js
@@ -0,0 +1,24 @@
+const btcPrice = prompt("What is Bitcoin price today");
+console.log(btcPrice);
+const baksS = prompt("How much $ do you have?");
+console.log(baksS);
+const btcBuy = baksS / btcPrice
+console.log(`You can buy ${btcBuy} BTC`)
+
+
+
+
+// Задача:
+// - користувач вводить курс Bitcoin до доллара
+// - користувач вводить суму доларів, що хоче поміняти на Bitcoin
+// - програма повертає кількість Bitcoin, що можна купити на вказану суму.
+
+// Приклад:
+
+// What is Bitcoin price today?
+// // 21000
+// // How much $ do you have?
+// // 1000
+// // You can buy 0.0476190 BTC
+
+
